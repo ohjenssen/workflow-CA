a33558dd4e1d3cdf943bdef88fd8979a096dea4e
ohjenssen <oskar_jenssen@hotmail.com>
2023-01-22 14:29:07 +0100

    test for login

---
+++ src/js/storage/storageMock.js
@@ -0,0 +1,22 @@
+//Taken from https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests
+export default class LocalStorageMock {
+    constructor() {
+      this.store = {};
+    }
+  
+    clear() {
+      this.store = {};
+    }
+  
+    getItem(key) {
+      return this.store[key] || null;
+    }
+  
+    setItem(key, value) {
+      this.store[key] = String(value);
+    }
+  
+    removeItem(key) {
+      delete this.store[key];
+    }
+}
