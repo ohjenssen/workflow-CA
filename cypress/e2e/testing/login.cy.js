60e6835a8900515ad15bbf63c20222e3080b072d
ohjenssen <oskar_jenssen@hotmail.com>
2023-01-24 15:43:21 +0100

    Test for login with invalid credentials

--- cypress/e2e/testing/login.cy.js
+++ cypress/e2e/testing/login.cy.js
@@ -1,6 +1,5 @@
 describe("login", () => {
   beforeEach(() => {
-    cy.wait(500);
     cy.visit("/");
   });
   it("can login with valid credentials", () => {
@@ -14,6 +13,24 @@ describe("login", () => {
       .submit()
       .should(() => {
         expect(localStorage.getItem("token")).to.exist;
+      });
+  });
+  it("cannot submit the login form with invalid credentials and is shown a message", () => {
+    cy.wait(500);
+    cy.get("#registerForm .btn-close").click();
+    cy.get(`header button[data-bs-target="#loginModal"]`).click();
+    cy.wait(500);
+    cy.get("#loginForm #loginEmail").type("invalidEmail@stud.norff.no");
+    cy.get("#loginForm #loginPassword").type("invalidPassword");
+    cy.get(`#loginForm`)
+      .submit()
+      .should(() => {
+        expect(localStorage.getItem("token")).to.be.null;
+        cy.on("window:alert", (str) => {
+          expect(str).to.equal(
+            "Either your username was not found or your password is incorrect"
+          );
+        });
       });
   });
 });
